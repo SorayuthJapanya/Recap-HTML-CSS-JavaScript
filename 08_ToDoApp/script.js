4f88ed96acbd70ff5978086173b53b9eac271464
SorayuthJapanya <sorayuthjaapanya@gmail.com>
2025-10-12 15:05:10 +0700

    feat(Todo Apps): build a Todo App
    - Styles with tailwind css (cdn)
    
    - Can Create and Delete Task
    
    - Can see a status task Active and Completed

---
+++ 08_ToDoApp/script.js
@@ -0,0 +1,174 @@
+const taskInput = document.getElementById("task-input");
+const addTaskBtn = document.getElementById("add-task");
+const todolist = document.getElementById("todo-list");
+const itemLeft = document.getElementById("items-left");
+const clearCompletedBtn = document.getElementById("clear-completed");
+const emptyState = document.getElementById("empty-state");
+const dateEl = document.getElementById("date");
+const filters = document.querySelectorAll(".filter");
+
+let todos = [];
+let currentFilter = "all";
+
+addTaskBtn.addEventListener("click", () => {
+  addTodo(taskInput.value);
+});
+
+taskInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") addTodo(taskInput.value);
+});
+
+clearCompletedBtn.addEventListener("click", clearCompleted);
+
+function addTodo(text) {
+  if (text.trim() === "") return;
+
+  const todo = {
+    id: Date.now(),
+    text,
+    completed: false,
+  };
+
+  todos.push(todo);
+
+  saveTodos();
+  renderTodos();
+}
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+  updateItemsCount();
+  checkEmptyState();
+}
+
+function updateItemsCount() {
+  const uncompletedTodos = todos.filter((todo) => !todo.completed);
+  itemLeft.textContent = `${uncompletedTodos.length} item${
+    uncompletedTodos.length !== 1 ? "s" : ""
+  } left`;
+}
+
+function checkEmptyState() {
+  const filteredTodos = filterTodos(currentFilter);
+  if (filteredTodos.length === 0) emptyState.classList.remove("hidden");
+  else emptyState.classList.add("hidden");
+}
+
+function filterTodos(filter) {
+  switch (filter) {
+    case "active":
+      return todos.filter((todo) => !todo.completed);
+    case "completed":
+      return todos.filter((todo) => todo.completed);
+    default:
+      return todos;
+  }
+}
+
+function renderTodos() {
+  todolist.innerHTML = "";
+
+  const filteredTodos = filterTodos(currentFilter);
+
+  filteredTodos.forEach((todo) => {
+    const todoItem = document.createElement("li");
+    todoItem.classList.add("todo-item");
+    todoItem.classList.add("group");
+    if (todo.completed) todoItem.classList.add("completed");
+
+    const checkboxContainer = document.createElement("label");
+    checkboxContainer.classList.add("checkbox-container");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("todo-checkbox");
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", () => toggleTodo(todo.id));
+
+    const checkmark = document.createElement("span");
+    checkmark.classList.add("checkmark");
+
+    checkboxContainer.appendChild(checkbox);
+    checkboxContainer.appendChild(checkmark);
+
+    const todoText = document.createElement("span");
+    todoText.classList.add("todo-item-text");
+    todoText.textContent = todo.text;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark">';
+    deleteBtn.addEventListener("click", () => deleteTodo(todo.id));
+
+    todoItem.appendChild(checkboxContainer);
+    todoItem.appendChild(todoText);
+    todoItem.appendChild(deleteBtn);
+
+    todolist.appendChild(todoItem);
+  });
+}
+
+function clearCompleted() {
+  todos = todos.filter((todo) => !todo.completed);
+  saveTodos();
+  renderTodos();
+}
+
+function toggleTodo(id) {
+  console.log(id);
+  todos = todos.map((todo) => {
+    if (todo.id === id) {
+      console.log(todo);
+      return { ...todo, completed: !todo.completed };
+    }
+
+    return todo;
+  });
+  saveTodos();
+  renderTodos();
+}
+
+function deleteTodo(id) {
+  todos = todos.filter((todo) => todo.id !== id);
+  saveTodos();
+  renderTodos();
+}
+
+function loadTodos() {
+  const storeTodos = localStorage.getItem("todos");
+  if (storeTodos) todos = JSON.parse(storeTodos);
+
+  checkEmptyState();
+  renderTodos();
+}
+
+filters.forEach((filter) => {
+  filter.addEventListener("click", () => {
+    setActiveFilter(filter.getAttribute("data-filter"));
+  });
+});
+
+function setActiveFilter(filter) {
+  currentFilter = filter;
+
+  filters.forEach((item) => {
+    if (item.getAttribute("data-filter") === filter) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
+    }
+  });
+
+  renderTodos();
+}
+
+function setDate() {
+  const options = { weekday: "long", month: "short", day: "numeric" };
+  const today = new Date();
+  dateEl.textContent = today.toLocaleDateString("en-EN", options);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadTodos();
+  setDate();
+});
